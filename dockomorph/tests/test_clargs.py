eb0e75b27b409061237a77184c854d418a6a4f62
Nathan Wilcox <nejucomo@gmail.com>
2016-04-23 08:01:48 -0700

    Add a test for unexpected commandline args.

--- dockomorph/tests/test_clargs.py
+++ dockomorph/tests/test_clargs.py
@@ -11,3 +11,6 @@ class parse_args_tests (LogMockingTestCase):
     def test_no_args(self):
         opts = clargs.parse_args('banana', [])
         self.assertEqual(opts, argparse.Namespace())
+
+    def test_unexpected_args(self):
+        self.assertRaises(SystemExit, clargs.parse_args, 'banana', ['slug'])
