70e5737396502c3b6eee5c252002655fd95204bf
Nathan Wilcox <nejucomo@gmail.com>
2016-05-07 16:43:37 -0700

    Change RootResource to a non-static.File which delegates to a static.File.

--- dockomorph/tests/test_web_root.py
+++ dockomorph/tests/test_web_root.py
@@ -1,20 +1,39 @@
-from mock import call
+from mock import call, sentinel
 
 from dockomorph.web.root import RootResource
 from dockomorph.tests.logutil import LogMockingTestCase
 
 
 class RootResourceTests (LogMockingTestCase):
+    def setUp(self):
+        LogMockingTestCase.setUp(self)
+
+        self.m_File = self.patch('twisted.web.static.File')
+        self.m_reactor = self.make_mock()
+
+        self.rr = RootResource(self.m_reactor)
+
     def test__init__(self):
-        m_File__init__ = self.patch('twisted.web.static.File.__init__')
-        m_reactor = self.make_mock()
-
-        rr = RootResource(m_reactor)
-
         self.assert_calls_equal(
-            m_reactor,
+            self.m_reactor,
             [])
 
         self.assert_calls_equal(
-            m_File__init__,
-            [call(rr, RootResource.StaticDir)])
+            self.m_File,
+            [call(RootResource.StaticDir)])
+
+    def test_getChild_delegates_to_statres(self):
+        m_statres = self.make_mock()
+        self.rr._statres = m_statres
+
+        result = self.rr.getChild(sentinel.path, sentinel.request)
+
+        self.assert_calls_equal(
+            self.m_reactor,
+            [])
+
+        self.assert_calls_equal(
+            m_statres,
+            [call.getChild(sentinel.path, sentinel.request)])
+
+        self.assertIs(result, m_statres.getChild.return_value)
--- dockomorph/web/root.py
+++ dockomorph/web/root.py
@@ -1,11 +1,21 @@
 import pkg_resources
 
-from twisted.web import static
+from twisted.web import resource, static
 
 
-class RootResource (static.File):
+class RootResource (resource.Resource):
     StaticDir = pkg_resources.resource_filename('dockomorph', 'web/static')
+
+    # Twisted API flags:
+    isLeaf = False
 
     def __init__(self, reactor):
         self._reactor = reactor
-        static.File.__init__(self, self.StaticDir)
+        resource.Resource.__init__(self)
+
+        self._statres = static.File(self.StaticDir)
+
+    def getChild(self, path, request):
+        # Note: This is only called when explicit putChild children
+        # aren't found, so those override self._statres:
+        return self._statres.getChild(path, request)
